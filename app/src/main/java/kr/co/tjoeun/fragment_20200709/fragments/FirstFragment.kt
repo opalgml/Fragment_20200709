e24bbac3a9336e387828bc97061ffd58e69084ac
조경진 <kj_cho@idealidea.co.kr>
2020-07-09 20:12:12 +0900

    프래그먼트 xml / 코틀린 클래스 연결

--- app/src/main/java/kr/co/tjoeun/fragment_20200709/fragments/FirstFragment.kt
+++ app/src/main/java/kr/co/tjoeun/fragment_20200709/fragments/FirstFragment.kt
@@ -1,9 +1,21 @@
 package kr.co.tjoeun.fragment_20200709.fragments
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import kr.co.tjoeun.fragment_20200709.R
 
 class FirstFragment : Fragment() {
 
 
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_first, container, false)
+    }
 
 }
