2ef6a02490fced26e91284662fe2412f37292a50
조경진 <kj_cho@idealidea.co.kr>
2020-07-09 20:17:32 +0900

    프래그먼트 동작 관련 코드 추가

--- app/src/main/java/kr/co/tjoeun/fragment_20200709/fragments/FirstFragment.kt
+++ app/src/main/java/kr/co/tjoeun/fragment_20200709/fragments/FirstFragment.kt
@@ -5,6 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import kotlinx.android.synthetic.main.fragment_first.*
 import kr.co.tjoeun.fragment_20200709.R
 
 class FirstFragment : Fragment() {
@@ -16,6 +17,14 @@ class FirstFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(R.layout.fragment_first, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+
+        changeMessageBtn.setOnClickListener {
+            welcomeMessageTxt.text = "버튼 실행이 잘 되었습니다."
+        }
     }
 
 }
